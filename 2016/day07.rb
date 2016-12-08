73e894d84d42dd04e1422d2d95226193d83490f6
Becojo <benoit.cote.jodoin@gmail.com>
2016-12-08 00:22:24 -0500

    day07

---
+++ 2016/day07.rb
@@ -0,0 +1,44 @@
+input = STDIN.read.strip.lines.map(&:strip)
+
+sum = 0
+sum2 = 0
+
+def check(str)
+  x = str[/(.)(.)\2\1/]
+  x && x[1] != x[-1]
+end
+
+def check2(hyper, outside)
+  outside.scan(/(?=([a-z])([a-z])(\1))/).each { |(a, b)|
+    if a != b && hyper[b + a + b]
+      return true
+    end
+  }
+
+  false
+end
+
+input.each do |ip|
+  parts = ip.split(/[\[\]]/)
+
+  hypernet = parts.select.each_with_index { |_, i| i.odd? }.join(',')
+  supernet = parts.select.each_with_index { |_, i| i.even? }.join(',')
+
+  sum2 += 1 if check2(hypernet, supernet)
+
+  is_okay = hypernet.all? do |p|
+    !check(p)
+  end
+
+  next unless is_okay
+
+  is_okay = supernet do |p|
+    check(p)
+  end
+
+  sum += 1 if is_okay
+
+end
+
+puts sum
+puts sum2
